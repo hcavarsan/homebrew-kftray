0f9866b00a1675c0bf72438f58c1e498878c3149
hcavarsan <hcavarsan@yahoo.com.br>
2024-04-10 19:08:46 -0300

    feat: improve formula

--- Formula/kftray-test.rb
+++ Formula/kftray-test.rb
@@ -12,10 +12,9 @@ class KftrayTest < Formula
     sha256 "01a7f4c4fbf2126657610132c144857665a70af37840bd5c1d4a2421d7f94b6a"
 
     def install
-      system "tar", "xvf", "kftray_universal.app.tar.gz"
       prefix.install "kftray.app"
       bin.install_symlink prefix/"kftray.app/Contents/MacOS/kftray"
-    end
+    endta
   end
 
   on_linux do

11884fef0ae551a34d662d5c64c6d0d26e69776f
hcavarsan <hcavarsan@yahoo.com.br>
2026-09-14 23:30:42 -0300

    fix: linux formula format

--- Formula/kftray-linux.rb
+++ Formula/kftray-linux.rb
@@ -4,21 +4,17 @@ class KftrayLinux < Formula
   desc "A cross-platform system tray app for Kubernetes port-forward management."
   homepage "https://github.com/hcavarsan/kftray"
   version "0.27.32"
+  url "https://github.com/hcavarsan/kftray/releases/download/v0.27.32/kftray_0.27.32_amd64.AppImage"
+  sha256 "b2404dc3282a9e953f24d8bd645b95c7f4400e885e3c0bf4df46e9f34c306495"
+
+  depends_on :linux
 
   NEWER_GLIBC_AMD64_SHA = "6c407f801fe2fc975ca31c64aa2cd2d7964337c60f9de8946124a35d3bd011ed"
   NEWER_GLIBC_ARM64_SHA = "6338b701be2fa5a03aefc2eb08ff47b9ebf94484da3545335cc8f6b24cdb0148"
 
-
-  on_linux do
-      on_intel do
-          url "https://github.com/hcavarsan/kftray/releases/download/v0.27.32/kftray_0.27.32_amd64.AppImage"
-          sha256 "b2404dc3282a9e953f24d8bd645b95c7f4400e885e3c0bf4df46e9f34c306495"
-      end
-
-      on_arm do
-          url "https://github.com/hcavarsan/kftray/releases/download/v0.27.32/kftray_0.27.32_aarch64.AppImage"
-          sha256 "0bac98838318ef308e8490ea35ee1629d3ca0195dad6c37c0828f6972dbe79b1"
-      end
+  on_arm do
+      url "https://github.com/hcavarsan/kftray/releases/download/v0.27.32/kftray_0.27.32_aarch64.AppImage"
+      sha256 "0bac98838318ef308e8490ea35ee1629d3ca0195dad6c37c0828f6972dbe79b1"
   end
 
   def install
